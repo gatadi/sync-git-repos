4a53343d6fd8266250cf0b5fab4e426529ffb6e7
gatadi <vijay.gatadi@hp.com>
2013-05-11 07:36:59 -0700

    fixing bat

--- clone-repos.bat
+++ clone-repos.bat
@@ -1,14 +1,14 @@
-cd c:\
-mkdir workarea
-cd workarea
-git clone http://github.com/gatadi/chef.git
-git clone http://github.com/gatadi/softwares.git
-git clone http://github.com/gatadi/projects.git
-git clone http://github.com/gatadi/old-stuff.git
-git clone http://github.com/gatadi/sync-git-repos.git
+@cd c:\
+@IF NOT EXIST workarea mkdir workarea
+@cd workarea
+@IF NOT EXIST chef call git clone http://github.com/gatadi/chef.git
+@IF NOT EXIST softwares  start call git clone http://github.com/gatadi/softwares.git
+@IF NOT EXIST projects  call git clone http://github.com/gatadi/projects.git
+@IF NOT EXIST old-stuff  call git clone http://github.com/gatadi/old-stuff.git
+@IF NOT EXIST sync-git-repos  call git clone http://github.com/gatadi/sync-git-repos.git
 
-echo "Cloning third party repos"
-mkdir third-party
-cd third-party
-git clone https://github.com/lognormal/boomerang.git
+@IF NOT EXIST third-party mkdir third-party
+@cd third-party
+@IF NOT EXIST boomerang  call git clone https://github.com/lognormal/boomerang.git
+@cd ..
 
